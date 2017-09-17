85c5e0d37f75cfbb1ce9e1618dece4efdba30e6c
Ali M <Alirsgp@gmail.com>
2017-09-17 01:09:05 -0400

    Update PracticeDelimiter.java

--- PracticeDelimiter.java
+++ PracticeDelimiter.java
@@ -1,15 +1,21 @@
 import java.util.Scanner;
 import java.io.File;
 import java.io.PrintWriter;
+import java.io.FileNotFoundException;
 
 public class PracticeDelimiter {
 	static File myFile = new File("crazyText.txt");
+	static File outFile = new File("outFile.txt");
+	static PrintWriter output;
 	private static Scanner uploadedFile;
-	public static void main(String [] args) {
+
+
+	public static void main(String [] args) throws FileNotFoundException {
 
 		try {
 		uploadedFile = new Scanner(myFile);
 		uploadedFile.useDelimiter("/|#|%|//n| ");
+		output = new PrintWriter(outFile);
 
 		} catch(Exception ex) {
 			System.out.println("File not found!");
@@ -18,7 +24,13 @@ public class PracticeDelimiter {
 		String myText = uploadedFile.next();
 		System.out.print(myText);
 		System.out.print(" ");
+		try {
+				output.print(myText + " ");
+			} catch (Exception e) {
+				System.out.println("Lol");
 			}
+		}
+		output.close();
 
 		System.out.println(); 
 		}
